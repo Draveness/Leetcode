af8757dd656c5ed9474ecd47259801f30a249453
Draveness <stark.draven@gmail.com>
2016-02-12 10:47:33 +0800

    add methods partial_match_table

--- 028_implement_strstr().rb
+++ 028_implement_strstr().rb
@@ -8,7 +8,7 @@ def str_str(haystack, needle)
     haystack_arr = haystack.split('')
     needle_arr = needle.split('')
     len = needle.length - 1
-    
+
     (0..haystack_arr.length).to_a.each do |hi|
         needle_arr.each_with_index do |s, ni|
             c = haystack_arr[hi + ni]
@@ -18,3 +18,37 @@ def str_str(haystack, needle)
     end
     -1
 end
+
+def partial_match_table(needle)
+    table = []
+
+    needle.partial_prefix_string(false).each do |prefix|
+        prefix_strings = prefix.partial_prefix_string
+        suffix_strings = prefix.partial_suffix_string
+        length = 0
+        prefix_strings.each do |prefix_string|
+             length = prefix_string.length if suffix_strings.include?(prefix_string) && prefix_string.length > length
+        end
+        table << length
+    end
+
+    table
+
+end
+
+class String
+    def partial_prefix_string(exclusive = true)
+        result = (1..self.length).to_a.map { |index| self.split("").take(index).join }
+        if exclusive
+            result[0..-2]
+        else
+            result
+        end
+    end
+
+    def partial_suffix_string
+        (1..self.length).to_a.map { |index| self.split("").drop(index).join }[0..-2]
+    end
+end
+
+puts partial_match_table("ababab")
